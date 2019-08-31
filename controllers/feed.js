b11f849ac8c0fb15ed29e00473f87e61f9281d33
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-31 19:48:43 -0300

    Lecture 399. The username is missing

--- controllers/feed.js
+++ controllers/feed.js
@@ -24,6 +24,7 @@ exports.getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate('creator')
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
