5a972a72dd1364bf410cf233b2f4b1e8f8c78d31
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-31 19:04:14 -0300

    Lecture 393. Clearing Post-User relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -176,6 +176,11 @@ exports.deletePost = (req, res, next) => {
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     }).then(result => {
+      return User.findById(req.userId);
+    }).then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    ).then(result => {
       console.log(result);
       res.status(200).json({ message: 'Deleted post.' });
     }).catch(err => {
