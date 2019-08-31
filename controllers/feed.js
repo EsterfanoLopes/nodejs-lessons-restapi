1e7da34a45bf328b2308b9e7bf97db071ead2578
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-30 23:10:31 -0300

    Lecture 377. Fetching a Single Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,19 +3,19 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/Book.jpg',
-        creator: {
-          name: 'Fulano',
-        },
-        createdAt: new Date(),
-      }],
-  });
+  Post.find()
+    .then(posts => {
+      res.status(200).json({
+        message: 'Fetched posts successfully',
+        posts,
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -37,6 +37,28 @@ exports.createPost = (req, res, next) => {
       res.status(201).json({
         message: 'Post created successfuly',
         post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find Post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: 'Post fetched.',
+        post,
       });
     })
     .catch(err => {
