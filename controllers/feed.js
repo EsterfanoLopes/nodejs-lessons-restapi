97cc46e922be43db8cd4c98f62b4cb1bb84ee6e3
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-30 22:13:19 -0300

    Lecture 371. Fatching Lists of Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,16 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      { 
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/Book.jpg',
+        creator: {
+          name: 'Fulano',
+        },
+        createdAt: new Date(),
+      }],
   });
 };
 
