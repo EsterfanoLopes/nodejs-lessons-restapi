9da1417898ff4d8ddfe28ad9f7c8bee9b778e95e
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-30 22:27:57 -0300

    Lecture 373. Adding Server Side Validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,7 +1,9 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
-      { 
+      {
         _id: '1',
         title: 'First Post',
         content: 'This is the first post!',
@@ -15,7 +17,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  // Create a post in db
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
+  }
   const { title, content } = req.body;
 
   res.status(201).json({
