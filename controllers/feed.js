ce14fc80ad821b5e6b2e3ef1e8b0ab9e4ffbb1a0
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-08-31 00:31:10 -0300

    Lecture 380 . Updating Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => {
+    console.log(err);
+  });
+};
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -66,6 +76,50 @@ exports.getPost = (req, res, next) => {
         message: 'Post fetched.',
         post,
       });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const { title, content } = req.body;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked');
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find Post');
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      post.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post updated!', post: result });
     })
     .catch(err => {
       if (!err.statusCode) {
--- routes/feed.js
+++ routes/feed.js
@@ -13,4 +13,9 @@ router.post('/post', [
 
 router.get('/post/:postId', feedController.getPost);
 
+router.put('/post/:postId', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+], feedController.updatePost);
+
 module.exports = router;
