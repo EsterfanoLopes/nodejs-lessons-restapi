cb2faffb0dcbe2de74438bad8877b39e103234ab
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-09-02 21:23:10 -0300

    Lecture 421. Adding input Validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
@@ -8,12 +9,23 @@ const envvars = process.env;
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
-    const existingUser = await User.findOne({ email: userInput.email });
+    const { email, name, password } = userInput;
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'E-mail is invalid.' });
+    }
+    if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+      errors.push({ message: 'Password too short!' });
+    }
+    if (errors.length > 0) {
+      throw new Error('Invalid input.');
+    }
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       const error = new Error('User exists already!');
       throw error;
     }
-    const { email, name, password } = userInput;
+
     const hashedPassword = await bcrypt.hash(password, parseInt(envvars.ENCRYPT_ROUNDS));
     
     const user = new User({ email, name, password: hashedPassword });
