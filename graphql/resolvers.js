34fbfcdb21ee176ae4fe893f096eab8157c28bf3
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-09-03 21:38:22 -0300

    Lecture 430. Adding a 'Get Post' Query and Resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -98,5 +98,28 @@ module.exports = {
       createdAt: createdPost.createdAt.toISOString(),
       updatedAt: createdPost.updatedAt.toISOString(),
     };
+  },
+  posts: async (args, req) => {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const totalPosts = await Post.find().countDocuments();
+    const posts = await Post
+      .find()
+      .sort({ createdAt: -1 })
+      .populate('creator')
+    return {
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString(),
+        };
+      }),
+      totalPosts,
+    }
   }
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -31,6 +31,11 @@ module.exports = buildSchema(`
     userId: String!
   }
 
+  type PostData {
+    posts: [Post!]!
+    totalPosts: Int!
+  }
+
   input PostInputData {
     title: String!
     content: String!
@@ -39,6 +44,7 @@ module.exports = buildSchema(`
 
   type RootQuery {
     login(email: String!, password: String!): AuthData!
+    posts: PostData!
   }
 
   type RootMutation {
