0d7e54e84a40ef530378e77d5828f51ed20d2d29
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-09-08 22:28:31 -0300

    Lecture 470. Setting up a Testing Database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
@@ -23,5 +25,24 @@ describe('Auth Controller - Login', function () {
     });
 
     User.findOne.restore();
+  });
+
+  it('Should send a response with a valid user status for an existing user', async (done) => {
+    dotenv.config();
+    const envvars = process.env;
+
+    const MONGODB_URI = `mongodb://${envvars.DB_USER}:${envvars.DB_PASSWORD}@ds219308.mlab.com:19308/suaaplicacao-api-test`;
+
+    mongoose
+      .connect(MONGODB_URI)
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+        });
+        user.save();
+      }).then(() => {}).catch(err => console.log(err));
   });
 });
